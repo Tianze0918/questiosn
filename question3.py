002b8ca3951a65f0d1202e5e10d76d2ee47bf92b
tianzezhao26@g.ucla.edu <160709907+Tianze0918@users.noreply.github.com>
2024-06-29 11:29:36 +0800

    syntax adjustment

--- question3.py
+++ question3.py
@@ -5,29 +5,33 @@ def median(num, list):
     return list[num//2]
 
 def max_sum_subarray(arr, k):
-    window_sum = sum(arr[:k])
-    max_sum = window_sum
-    for i in range(1, len(arr)-k+1):
-        window_sum = window_sum - arr[i-1] + arr[i+k-1]
-        max_sum = max(max_sum, window_sum)
+    max_result = 0
+    for start in range(len(arr) - k + 1):
+        subarr = arr[start:start + k]
+        subarr_sum = sum(subarr)
+        subarr_median = median(k, subarr)
+        temp_result = (subarr_sum+k-1)//k - subarr_median
+        max_result = max(max_result, temp_result)
     
-    return max_sum
+    return max_result
 
 def maxval(sequence):
     result=0
     if len(sequence) < 2:
         return 0 
-    tmain=0
-    tmedian=0
     sorted_seq=sorted(sequence)
     window=2
     while (window<=len(sequence)):
-        tmain=main_num(window, max_sum_subarray(sorted_seq,window))
-        tmedian=median(window, sorted_seq)
-        temp_result=tmain-tmedian
+        temp_result=max_sum_subarray(sorted_seq,window)
         if temp_result>result:
             result=temp_result
         window+=1
     return result
 
 
+def run():
+    sequence = [1, 3, 9]
+    print(maxval(sequence))
+
+if __name__ == "__main__":
+    run()
